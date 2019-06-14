bb4055f487ab0b72145cf6bd38de011ffe420058
isio666 <a.umrilov88@yandex.ru>
2019-06-14 17:52:26 +0300

    еще один тестовый комит

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!!");
+            int x = 0;
             Console.ReadKey();
         }
     }
